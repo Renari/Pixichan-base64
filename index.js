4becd434512ffcec2938bc5399538865e9f8e657
adefirmanf <syskeys32@gmail.com>
2018-08-04 16:24:36 +0700

    Release alpha version

--- index.js
+++ index.js
@@ -1,38 +1,70 @@
-/**
- * Module dependecies
- * @private 
- */
-const Request = require('request-promise')
-const { Url } = require('url')
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable arrow-body-style */
 
 /**
- * Represent of PixiChan 
+ * Module dependecies
+ * @private
+ */
+
+const Request = require('request-promise');
+
+/**
+ * Represent of PixiChan
  * @constructor
  * @param {string} url - URL of pixiv images
  */
 
-class PixiChan{
-   constructor(url, opts = {value : 'image'}){
-      const _re = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/;
-      if(typeof url !== 'string'){
-         try{
-            throw new TypeError(`Expected string but got ${typeof(url)}`)
-         }
-         catch(e){
-            console.log(e)
-         }
+/**
+  * Represent of Symbol to create private scope
+  */
+const __url = Symbol('url');
+
+/**
+ * @class PixiChan
+ */
+class PixiChan {
+  constructor(url) {
+    const _re = /(http(s?):)([/|.|\w|\s|-])*\.(?:jpg|gif|png)/;
+    if (typeof url !== 'string') {
+      try {
+        throw new TypeError(`Expected string but got ${typeof (url)}`);
+      } catch (e) {
+        console.log(e);
       }
-      if(!_re.test(url)){
-         try{
-            throw new TypeError(`Invalid URL`)
-         }
-         catch(e){
-            console.log(e)
-         }
+      return;
+    }
+    if (!_re.test(url)) {
+      try {
+        throw new TypeError('Invalid URL of Pixiv image. ');
+      } catch (e) {
+        console.log(e);
       }
-   }
+      return;
+    }
+    /** @protected */
+    this[__url] = url;
+    // this = opts;
+  }
+
+  init() {
+    return new Promise((resolve, reject) => {
+      const R = Request.defaults({
+        headers: {
+          referer: 'https://www.pixiv.net',
+        },
+        encoding: null,
+      });
+      R.get(this[__url]).then((response) => {
+        const Img = Buffer.from(response, 'binary').toString('base64');
+        resolve(Img);
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  }
 }
-/*
-   PixiChan(url, options)
-      .then((results)=> log(results))
-*/
+
+module.exports = (url) => {
+  return new PixiChan(url)
+    .init();
+};
